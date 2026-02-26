a8b6c1439346a8791d01f36ee2793d831c569e3b
Jeremiah Andrews <jeremiah.i.andrews@outlook.com>
2026-02-26 14:31:11 -0800

    Initial Files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const map = L.map('map').setView([36, -94], 4.1);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/jandre-pro/cmlwyuil3000401sm8dnye6eg/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamFuZHJlLXBybyIsImEiOiJjbWxmaTFsOTIwMjY5M2VvaWIzbWgyb3F2In0.6E3iTTICdzYRJcjiQGGCMQ', {
+        maxZoom: 19,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+}).addTo(map);
+var logo = L.control({position: 'bottomleft'});
+
+logo.onAdd = function(map) {
+  var div = L.DomUtil.create('div', 'mapbox-logo');
+  div.innerHTML = '<img src="img/mapbox-logo-white.png" width="100"/>';
+  return div;
+};
+
+logo.addTo(map);
